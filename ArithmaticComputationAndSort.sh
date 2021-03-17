61325efbfbc52b188b2bb42b48e9722adc16208f
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-17 23:33:06 +0530

    Add: Results stored in a dictionary

--- ArithmaticComputationAndSort.sh
+++ ArithmaticComputationAndSort.sh
@@ -3,7 +3,12 @@ read -p "Enter value a: " a
 read -p "Enter value b: " b
 read -p "Enter value c: " c
 
-A=$((a+b*c))
-B=$((a*b+c))
-C=`echo "scale=1;$c+$a/$b" | bc`
-D=$((a%b+c))
+declare -A Results
+
+Results[A]=$((a+b*c))
+Results[B]=$((a*b+c))
+Results[C]=`echo "scale=1;$c+$a/$b" | bc`
+Results[D]=$((a%b+c))
+
+echo ${Results[@]}
+
